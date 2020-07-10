e3d93df8268826a90fe4255a1f3eb6582a124d06
Etienne Jannin <etienne.jannin@gmail.com>
2020-07-10 10:19:56 -0400

    form update

--- logon.sh
+++ logon.sh
@@ -20,9 +20,12 @@ UNDERLINE=$(tput smul)
 Print_Style() {
   printf "%s\n" "${2}$1${NORMAL}"
 }
+
 echo
 Print_Style "Welcome to $(hostname)!" "$BRIGHT"
 echo
+
+# Setting the variables
 UNAME=$(uname -a)
 TotalMemory=$(awk '/MemTotal/ { printf "%.0f\n", $2/1024 }' /proc/meminfo)
 AvailableMemory=$(awk '/MemAvailable/ { printf "%.0f\n", $2/1024 }' /proc/meminfo)
@@ -32,21 +35,30 @@ CPUTemp=$(vcgencmd measure_temp|cut -d"=" -f2)
 CPUFreq=$(vcgencmd get_config arm_freq|cut -d"=" -f2)
 IPaddr=$(hostname -I)
 DefaultGateway=$(ip route show|head -1|awk '{ print $3 }')
+
 Print_Style "$UNAME" "$YELLOW"
 uptime
+
+# Minecraft
 echo
 Print_Style "MINECRAFT" "$RED"
 mcrcon -H $IPaddr -p C0sm0s1826 "list"
+
+# Memory
 echo
 Print_Style "MEMORY" "$RED"
 Print_Style "Total memory: $TotalMemory - Available Memory: $AvailableMemory - Used Memory: $UsedMemory" "$NORMAL"
 Print_Style "Percentage used memory: $PercentUsed%" "$NORMAL"
+
+# CPU
 echo
 Print_Style "CPU" "$RED"
 Print_Style "Frequency: $CPUFreq Mhz"
 Print_Style "Temperature: $CPUTemp" "$NORMAL"
 Print_Style "Top 10 ressource intensive processes:"
 ps -eo pcpu,pid,user,args | sort -k 1 -r | head -10
+
+# Network
 echo
 Print_Style "NETWORK" "$RED"
 Print_Style "IP address: $IPaddr" "$NORMAL"
