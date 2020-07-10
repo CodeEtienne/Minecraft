51c20870967f47f60f7815ee0b396bcbce3467eb
Etienne <etienne.jannin@gmail.com>
2020-07-10 07:52:41 -0400

    Update MinecraftStats.py

--- MinecraftStats.py
+++ MinecraftStats.py
@@ -100,14 +100,15 @@ for file in files(statDir):
 # 3 - Generating the csv file
 
 stat_names = []
-players = sorted(stats.keys())
 
 # Gets a list of player names
+players = sorted(stats.keys())
+
+# Gets a list of stat names
 for player in stats:
     for row in stats[player]:
         stat_names.append(row)
 
-# Gets a list of stat names
 stat_names = sorted(list(set(stat_names)))
 
 # onto the csv file
